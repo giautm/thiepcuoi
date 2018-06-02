5a4e236fa684656d7c45407b5e54c9be7c4b6079
giautm <giau.tran@tiki.vn>
2018-06-03 01:58:45 +0700

    Added GA

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Head, Main, NextScript } from 'next/document'
+import { GA_ID } from '../lib/metrics'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -17,6 +18,20 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript/>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_ID}');
+              `
+            }}
+          />
         </body>
       </html>
     )
